4570b8f48fdaa6daa6fd6320b56786c6280ef113
Renzo <dankoshevoy2001@gmail.com>
2022-06-01 02:39:31 +0300

    best

---
+++ Notebook/Notebook.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include "/Users/danko/source/repos/Renzo-Dev/Renzo_Library/Renzo_Library/Renzo_Library/RenzoLib.h"
+
+
+using namespace renzo;
+
+struct Account
+{
+	string name; // Имя
+	string surname; // Фамилия
+	string email; // Почта
+
+
+	// Дата Рождения
+	struct BirthDay 
+	{
+		int day = 01;
+		int month = 01;
+		int yaer = 2001;
+	};
+
+};
+
+
+class Notebook
+{
+public:
+
+
+
+};
